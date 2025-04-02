7cdb74ede506f3c6c3632e70f275f8e1e3b17b54
hsmt72k <hsmt72k@gmail.com>
2025-04-03 04:20:16 +0900

    09. Chat Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import { Button } from '@/components/ui/button';
+import { Chat } from '@/components/chat';
 
 export default function Home() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center gap-6">
-        <h1 className="font-extrabold">RAG を使った AI チャットアプリ</h1>
-        <Button
-          size="xl"
-          variant="destructive"
-          className="text-6xl font-host-grotesk hover:cursor-pointer hover:opacity-60"
-        >
-          Click me!
-        </Button>
-      </div>
-    </div>
-  );
+  return <Chat />;
 }
