605d589963a395e191428fdbd9e287220aaf7adc
hsmt72k <hsmt72k@gmail.com>
2025-04-03 01:26:23 +0900

    04. Setup Next.js Project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="font-extrabold">RAG を使った AI チャットアプリ</h1>
+    </div>
+  )
+}
