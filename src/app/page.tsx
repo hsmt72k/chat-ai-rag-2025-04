1fb7d8e5b4c2609ffc30945fd73c286d03e56f77
hsmt72k <hsmt72k@gmail.com>
2025-04-03 02:19:32 +0900

    05. Setup shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1 className="font-extrabold">RAG を使った AI チャットアプリ</h1>
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="font-extrabold">RAG を使った AI チャットアプリ</h1>
+        <Button
+          size="xl"
+          variant="destructive"
+          className="text-6xl font-host-grotesk hover:cursor-pointer hover:opacity-60"
+        >
+          Click me!
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
